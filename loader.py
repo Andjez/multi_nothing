841a785992675bccfe0d900caa8a18b121e64d18
Andjez <92543526+Andjez@users.noreply.github.com>
2023-06-13 14:49:33 +0530

    loader.py

--- loader.py
+++ loader.py
@@ -13,21 +13,36 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from youtube_transcript_api import YouTubeTranscriptApi
 import pikepdf
+from PyPDF2 import PdfReader
 from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader
 
 def pdf_loader(uploaded_files):
-    pdf_text = {}
+    #pdf_text = {}
     for file in uploaded_files:
         bytes_data = file.read()
         pdf = pikepdf.open(file)
-        pdf.save(file.name)
-        filename = file.name
-        loader = UnstructuredPDFLoader(filename)
-        data = loader.load()
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
-        texts = text_splitter.split_documents(data)
-        pdf_text.update(texts)
-    return pdf_text
+      #  pdf.save(file.name)
+      #  filename = file.name
+      #  loader = UnstructuredPDFLoader(filename)
+      #  data = loader.load()
+      #  text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
+      #  texts = text_splitter.split_documents(data)
+      #  pdf_text.update(texts)
+    #return pdf_text
+    raw_text = ''
+    doc_reader = PdfReader(pdf)
+    for i, page in enumerate(doc_reader.pages):
+        text = page.extract_text()
+        if text:
+            raw_text += text
+    text_splitter = CharacterTextSplitter(        
+    separator = "\n",
+    chunk_size = 1000,
+    chunk_overlap  = 200, #striding over the text
+    length_function = len,
+     )
+    texts = text_splitter.split_text(raw_text)
+    return texts
 
 def youtube_loader(yt_link):
     loader = YoutubeLoader.from_youtube_url(yt_link, add_video_info=True)
