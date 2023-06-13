69fbc4b84d9daa23d293eb11422c0e66df1ebf97
Andjez <92543526+Andjez@users.noreply.github.com>
2023-06-13 23:57:40 +0530

    pdf_page.py

--- pages/pdf_page.py
+++ pages/pdf_page.py
@@ -8,22 +8,22 @@
 # Copyright:   (c) Androse_Jes 2023
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-
-import os, re
+import os, re, time
 import streamlit as st
+from io import BytesIO
+import main, loader, faiss
+from pypdf import PdfReader
 from langchain import OpenAI
-#from apikey import openai_apikey
+from langchain.llms import OpenAI
 from streamlit_chat import message
+from typing import Any, Dict, List
+from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
-import main
-import loader
+from langchain.docstore.document import Document
 from langchain.chains import VectorDBQAWithSourcesChain
 from langchain.chains import RetrievalQAWithSourcesChain
-
-import faiss
-from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 if "generated_01" not in st.session_state:
@@ -31,7 +31,10 @@ if "generated_01" not in st.session_state:
 
 if "past_01" not in st.session_state:
     st.session_state["past_01"] = []
-st.set_page_config(page_title="PDF Chatbot", page_icon=":zany_face:")
+chain = None
+pages = None
+#st.set_page_config(page_title="PDF Chatbot", page_icon=":zany_face:")
+
 st.markdown("# PDF Chatbot 📕📖")
 st.sidebar.markdown("# PDF Chatbot 📕📖")
 
@@ -45,36 +48,107 @@ with col1:
     # Clears all st.cache_resource caches:
         st.session_state["link"] = ""
         st.cache_resource.clear()
+        st.session_state["generated_01"] = []
+        st.session_state["past_01"] = []
+        st.session_state["source"] = []
+        st.session_state["time_sec"] = []
+        st.session_state["foo_"] = ""
+        st.cache_data.clear()
 with col2:
     if st.button("Clear History"):
         st.session_state["generated_01"] = []
         st.session_state["past_01"] = []
         st.session_state["source"] = []
         st.session_state["time_sec"] = []
-        st.session_state["foo"] = ""
+        st.session_state["foo_"] = ""
 
-@st.cache_resource
-def load_chain(texts):
+#new lines
+
+@st.cache_data
+def parse_pdf(file: BytesIO) -> List[str]:
+    pdf = PdfReader(file)
+    output = []
+    for page in pdf.pages:
+        text = page.extract_text()
+        # Merge hyphenated words
+        text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
+        # Fix newlines in the middle of sentences
+        text = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", text.strip())
+        # Remove multiple newlines
+        text = re.sub(r"\n\s*\n", "\n\n", text)
+        output.append(text)
+    return output
+
+
+@st.cache_data
+def text_to_docs(text: str) -> List[Document]:
+    """Converts a string or list of strings to a list of Documents
+    with metadata."""
+    if isinstance(text, str):
+        # Take a single string as one page
+        text = [text]
+    page_docs = [Document(page_content=page) for page in text]
+
+    # Add page numbers as metadata
+    for i, doc in enumerate(page_docs):
+        doc.metadata["page"] = i + 1
+
+    # Split pages into chunks
+    doc_chunks = []
+
+    for doc in page_docs:
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=4000,
+            separators=["\n\n", "\n", ".", "!", "?", ",", " ", ""],
+            chunk_overlap=0,
+        )
+        chunks = text_splitter.split_text(doc.page_content)
+        for i, chunk in enumerate(chunks):
+            doc = Document(
+                page_content=chunk, metadata={"page": doc.metadata["page"], "chunk": i}
+            )
+            # Add sources a metadata
+            doc.metadata["source"] = f"{doc.metadata['page']}-{doc.metadata['chunk']}"
+            doc_chunks.append(doc)
+    return doc_chunks
+
+
+@st.cache_data
+def test_embed(_pages):
     instructor_embeddings = main.embedding()
-    store = FAISS.from_documents(documents=texts,embedding=instructor_embeddings)
-    retriever = store.as_retriever(search_kwargs={"k": 3})
+    # Indexing
+    # Save in a Vector DB
+    with st.spinner("It's indexing..."):
+        index = FAISS.from_documents(pages, instructor_embeddings)
+        #store = FAISS.from_documents(documents=texts,embedding=instructor_embeddings)
+        retriever = index.as_retriever(search_kwargs={"k": 3})
+    st.success("Embeddings done.", icon="✅")
+
     return retriever
 
-#yt_link = st.text_input("enter youtube link here!",key="link")
+uploaded_file = st.file_uploader("**Upload Your PDF File**", type=["pdf"])
 
-uploaded_files = st.file_uploader(f"Choose PDF file(s) to Upload", accept_multiple_files=True,key="link_01")
-
-def get_text():
-    input_text = st.text_input("You: ", key="foo")
-    return input_text
-if uploaded_files:
-    texts = loader.pdf_loader(uploaded_files)
-    chain = load_chain(texts)
+if uploaded_file:
+    name_of_file = uploaded_file.name
+    doc = parse_pdf(uploaded_file)
+    pages = text_to_docs(doc)
+    chain = test_embed(pages)
     model = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
-    user_input = get_text()
+else:
+    st.session_state["generated_01"] = []
+    st.session_state["past_01"] = []
+    st.session_state["source"] = []
+    st.session_state["time_sec"] = []
+    st.session_state["foo_"] = ""
+
+if pages:
+    with st.expander("Show Page Content", expanded=False):
+        page_sel = st.number_input(label="Select Page", min_value=1, max_value=len(pages), step=1)
+        pages[page_sel - 1]
+if chain:
+    user_input = st.text_input("You: ", key="foo_")
 
 if user_input:
-    #add_video = st.video(yt_link,start_time=0)
     docs = chain.get_relevant_documents(user_input)
     output = docs[0].page_content
     answer = model.run(input_documents=docs, question=user_input)
