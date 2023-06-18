507500d85d2f579cc9ea7f7b54d32bbb49dc8a36
Andjez <92543526+Andjez@users.noreply.github.com>
2023-06-18 22:18:34 +0530

    1_📚_pdf_chat.py

--- pages/1_📚_pdf_chat.py
+++ pages/1_📚_pdf_chat.py
@@ -112,14 +112,11 @@ def text_to_docs(text: str) -> List[Document]:
             doc_chunks.append(doc)
     return doc_chunks
 
-@st.cache_resource
-def emb_model():
-    inst_embeddings = main.embedding()
-    return inst_embeddings
+
     
 @st.cache_resource
 def test_embed(_pages):
-    instructor_embeddings = emb_model()
+    instructor_embeddings = main.embedding()
     # Indexing
     # Save in a Vector DB
     with st.spinner("It's indexing..."):
